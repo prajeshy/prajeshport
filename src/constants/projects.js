93f0081483ad4d017eb299028de0d9627a6cee35
Prajesh Yeotikar <yeotikarprajesh@gmail.com>
2021-10-07 21:42:23 +0530

    Add files via upload

---
+++ src/constants/projects.js
@@ -0,0 +1,128 @@
+export const PROJECTS = [
+  {
+    id: '1',
+    title: 'Ethical Hacking and Cybersecurity',
+    description:
+      '',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://raw.githubusercontent.com/prajeshy/certs/main/EHCS.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     
+    ],
+  },
+  {
+    id: '2',
+    title: 'Linux for Network Engineers Practical Linux with GSN3',
+    description:
+     '',
+    image: {
+      src: 'https://raw.githubusercontent.com/prajeshy/certs/main/UC-3f65e0e3-fac4-4047-b7e4-19039ee1afd6.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://yourplaces-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/MERN-places',
+    },
+    technologies: [
+      
+    ],
+  },
+  {
+    id: '3',
+    title: 'Complete CCNP ENCOR (350-401) Master Class',
+    description:
+     '',
+    image: {
+      src: 'https://raw.githubusercontent.com/prajeshy/certs/main/UC-ad08f0fa-55ff-4416-82f4-e8903e8d0ade-2.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://expo.dev/@v_snaichuk/online-shop-app',
+      repo: 'https://github.com/vsnaichuk/RN-shop',
+    },
+    technologies: [
+      
+    ],
+  },
+  {
+    id: '4',
+    title: 'Usable Security',
+    description:
+      '',
+    image: {
+      src: 'https://raw.githubusercontent.com/prajeshy/certs/main/Coursera%20JDPGRJJXUTTR.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://expo.dev/@v_snaichuk/rn-places',
+      repo: 'https://github.com/vsnaichuk/RN-places',
+    },
+    technologies: [
+     
+    ],
+  },
+  {
+    id: '5',
+    title: 'Object Orineted Programming in Java',
+    description:
+      '',
+    image: {
+      src: 'https://raw.githubusercontent.com/prajeshy/certs/main/Coursera%20W23HYFYD5TCW-2-1.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://shopping-cart-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/React-ShoppingCart',
+    },
+    technologies: [
+      
+    ],
+  },
+  {
+    id: '6',
+    title: 'Java Programming : Solving Problem using Software',
+    description:
+    '',
+    image: {
+      src: 'https://raw.githubusercontent.com/prajeshy/certs/main/probmlem%20solving%20java-1.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://shopping-cart-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/React-ShoppingCart',
+    },
+    technologies: [
+    ],
+  },
+  {
+    id: '7',
+    title: 'C++ : From Beginner to Expert',
+    description:
+   '',
+    image: {
+      src: 'https://raw.githubusercontent.com/prajeshy/certs/main/cpp.jpeg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://shopping-cart-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/React-ShoppingCart',
+    },
+    technologies: [
+    
+    ],
+  },
+];
